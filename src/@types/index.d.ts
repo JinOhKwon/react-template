b5f65db57f247cf16858a5863e9db99969040a45
권진오 <kjo1333@gmail.com>
2023-03-06 05:29:32 +0900

    설정 수정

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -3,25 +3,3 @@
  *
  * @description exmaple select type, checkbox type etc... 화면 기준입니다.
  */
-
-/**
- * 사이드바 타입
- */
-export interface SidebarType {
-  /**
-   * 식별자
-   */
-  uuid: string;
-  /**
-   * 주소
-   */
-  path: string;
-  /**
-   * 제목
-   */
-  title: string;
-  /**
-   * 아이콘
-   */
-  icon?: ReactNode;
-}
